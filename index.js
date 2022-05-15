6d43f71e4273428b7466e14663d983bfc327ef06
jeison iglesias <jeisonmavisoyface.com>
2022-05-15 11:57:20 -0500

    template engine added

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');//concatenar rutas o direcciones de directorios
+const ejs = require('ejs');
+
 const app =express();
-app.get('/', (req,res) => res.send("Hello World"))
+
+// settings
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/saludo', (req,res) => res.send("Hello World saludo"));
+
+app.get('/profile',(req, res) =>{
+    res.render('profile',{
+        name:'jeison',
+        age:23
+    })
+})
+//static files
+app.use(express.static('public'))
+
+// starting the server 
 app.listen(process.env.PORT || 3000);
 console.log(`Server on port ${process.env.PORT || 3000}`)
